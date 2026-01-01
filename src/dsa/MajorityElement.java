c4dc33589158183433ef37faa0d1d861beeaba65
Abhijit <settbros@yahoo.in>
2026-01-01 09:29:03 +0530

    Majority Element

--- src/dsa/MajorityElement.java
+++ src/dsa/MajorityElement.java
@@ -1,5 +1,38 @@
+package dsa;
+
 class MajorityElement{
     public static void main(String[] args) {
+        int[] arr = {1, 1, 2, 1, 3, 5, 1};
+        System.out.println(majorityElement(arr));
+    }
 
+    public static int majorityElement(int[] arr){
+        int candidate = -1;
+        int n = arr.length;
+        int count = 0;
+
+        for(int num : arr){
+            if(count == 0){
+                candidate = num;
+                count = 1;
+            }else if(candidate == num){
+                count++;
+            }else{
+                count--;
+            }
+        }
+
+        // validate the candidate
+        count = 0;
+        for(int num:arr){
+            if(candidate == num)
+                count++;
+        }
+
+        if(count > n/2){
+            return candidate;
+        }else{
+            return -1;
+        }
     }
 }
